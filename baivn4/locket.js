afde85c86fa849cca2bee592f850f485408fbf60
chinhlocker <chinhminh2501@gmail.com>
2025-05-23 10:56:53 +0700

    bvnbuoi7

---
+++ baivn4/locket.js
@@ -0,0 +1,51 @@
+document.body.style.margin = "0";
+document.body.style.background = "black";
+document.body.style.fontFamily = "sans-serif";
+document.body.style.color = "white";
+document.body.style.display = "flex";
+document.body.style.flexDirection = "column";
+document.body.style.alignItems = "center";
+
+var a = document.getElementById("a");
+a.style.marginTop = "30px";
+a.style.textAlign = "center";
+
+var b = document.getElementById("b");
+b.style.fontSize = "20px";
+b.style.marginBottom = "20px";
+
+var c = document.getElementById("c");
+c.style.width = "200px";
+c.style.height = "400px";
+c.style.background = "#111";
+c.style.borderRadius = "30px";
+c.style.padding = "10px";
+c.style.position = "relative";
+
+var d = document.getElementById("d");
+d.style.width = "100%";
+d.style.height = "60%";
+d.style.background = "black";
+d.style.borderRadius = "20px";
+
+var e = document.getElementById("e");
+e.style.width = "100%";
+e.style.height = "100%";
+e.style.objectFit = "cover";
+
+var f = document.getElementById("f");
+f.style.marginTop = "20px";
+f.style.display = "flex";
+f.style.justifyContent = "space-around";
+f.style.alignItems = "center";
+
+var g = document.getElementById("g");
+g.style.width = "40px";
+g.style.height = "40px";
+g.style.background = "white";
+g.style.borderRadius = "50%";
+g.style.border = "4px solid #aaa";
+
+var h = document.getElementById("h");
+h.style.marginTop = "10px";
+h.style.fontSize = "12px";
---
+++ baivn4/mail.js
@@ -0,0 +1,29 @@
+document.body.style.fontFamily = "sans-serif";
+document.body.style.background = "#f9f9f9";
+document.body.style.padding = "40px";
+
+let a = document.getElementById("a");
+a.style.background = "white";
+a.style.maxWidth = "600px";
+a.style.margin = "auto";
+a.style.padding = "30px";
+a.style.borderRadius = "8px";
+a.style.boxShadow = "0 2px 6px rgba(0,0,0,0.1)";
+a.style.textAlign = "center";
+
+let b = document.getElementById("b");
+b.style.color = "#2e2e2e";
+
+let c = document.getElementById("c");
+c.style.background = "#5865F2";
+c.style.color = "white";
+c.style.padding = "12px 24px";
+c.style.borderRadius = "5px";
+c.style.textDecoration = "none";
+c.style.display = "inline-block";
+c.style.marginTop = "20px";
+
+let d = document.getElementById("footer");
+d.style.fontSize = "13px";
+d.style.color = "#777";
+d.style.marginTop = "30px";
